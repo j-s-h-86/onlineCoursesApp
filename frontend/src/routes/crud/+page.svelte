66884ca46e5f18e5e2ad9a7d2de34caa5d5a633b
johan.hammarstedt@j-s-h.com <johan.hammarstedt@j-s-h.com>
2025-01-02 16:16:28 +0100

    implemented logout function

--- frontend/src/routes/crud/+page.svelte
+++ frontend/src/routes/crud/+page.svelte
@@ -1,1 +1,27 @@
+<script>
+	import { user } from '../../lib/stores';
+
+	const baseURL = import.meta.env.VITE_API_BASE_URL;
+
+	async function logoutFunction() {
+		const response = await fetch(`${baseURL}/logout`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+
+		const result = await response.json();
+		if (result.success) {
+			console.log('Logout successful:', result.message);
+			user.set(null);
+			location.href = '/';
+		} else {
+			console.error('Logout failed:', result.message);
+		}
+	}
+</script>
+
 <h1>CRUD</h1>
+
+<button on:click={logoutFunction}>Log out</button>
